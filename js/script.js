1537bb86dc9b762a41bd53b52ac16222c87b439b
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-12-13 10:10:06 +0100

    interval created, exercise done

--- js/script.js
+++ js/script.js
@@ -104,3 +104,7 @@ const next_button = document.querySelector('.next');
 next_button.addEventListener('click', next);
 
 prev_button.addEventListener('click', prev);
+
+// 1 - creare un intervallo che esegui la funzione 'next' ogni 5 secondi
+
+let autoplay = setInterval(next, 5000);
